41b54bb78a71628ba5063ae494e597160082f102
ventsi <v.saraminev@abv.bg>
2020-04-22 21:59:02 +0300

    Init

---
+++ users/schema/schema.js
@@ -0,0 +1,29 @@
+const graphql = require('graphql');
+
+const { 
+   GraphQLObjectType,
+   GraphQLString,
+   GraphQLInt
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+   name: 'User',
+   fields: {
+      id: { type: GraphQLString },
+      firstName: { type: GraphQLString },
+      age: { type: GraphQLInt }
+   }
+});
+
+const RootQuery = new GraphQLObjectType({
+   name: 'RootQueryType',
+   fields: {
+      user: {
+         type: UserType,
+         args: { id: { type: GraphQLInt } },
+         resolve(parentValue, args) {
+            
+         }
+      }
+   }
+});
