9b860a5dce170d5f2b575f7f8748409c96a85c17
ventsi <v.saraminev@abv.bg>
2020-05-01 21:38:46 +0300

    added lyrics to song

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
    render() {
+      const { song } = this.props.data;
+
+      if (!song) { return <div>Loading...</div>; }
       return (
          <div>
-            <h3>Song Detail</h3>
+            <Link to="/">Back</Link>
+            <h3>{song.title}</h3>
+            <LyricList lyrics={song.lyrics}/>
+            <LyricCreate songId={song.id} />
          </div>
       )
    }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+   options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
