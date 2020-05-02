6731058b356e05334755afdc55075422b636e306
ventsi <v.saraminev@abv.bg>
2020-05-02 10:36:29 +0300

    Added login and logout functionality

--- auth-graphql-starter/server/schema/mutations.js
+++ auth-graphql-starter/server/schema/mutations.js
@@ -15,8 +15,26 @@ const mutation = new GraphQLObjectType({
             email: { type: GraphQLString },
             password: { type: GraphQLString }
          },
-         resolve(parentValue, { email, password, req }) {
+         resolve(parentValue, { email, password }, req) {
             return AuthService.signup({ email, password, req });
+         }
+      },
+      logout: {
+         type: UserType,
+         resolve(parentValue, args, req) {
+            const { user } = req;
+            req.logout();
+            return user;
+         }
+      },
+      login: {
+         type: UserType,
+         args: {
+            email: { type: GraphQLString },
+            password: { type: GraphQLString }
+         },
+         resolve(parentValue, { email, password }, req) {
+            return AuthService.login({ email, password, req });
          }
       }
    }
