3ae8ec00e80785cd29e7841d0617845aa1678422
ventsi <v.saraminev@abv.bg>
2020-05-03 19:23:16 +0300

    working on signup/login forms

---
+++ auth-graphql-starter/client/components/SignupForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Signup';
+
+class SignupForm extends Component {
+   onSubmit({ email, password }) {
+      this.props.mutate({
+         variables: {
+            email,
+            password
+         }
+      });
+   }
+
+   render() {
+      return (
+         <div>
+            <h3>Sign Up</h3>
+            <AuthForm 
+               errors={[]} 
+               onSubmit={this.onSubmit.bind(this)}
+            />
+         </div>
+      );
+   }
+}
+
+export default graphql(mutation)(SignupForm);
--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Route, Router, hashHistory, IndeRoute } from 'react-router';
+import App from './components/App';
+import LoginForm from './components/LoginForm';
+import Signup from './components/SignupForm';
+import SignupForm from './components/SignupForm';
+
+const networkInterface = createNetworkInterface({
+  uri:'/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
+const client = new ApolloClient({
+  networkInterface,
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client} >
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <Route path="login" component={LoginForm} />
+          <Route path="signup" component={SignupForm} />
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
